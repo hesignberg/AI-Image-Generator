ef54988833358104192969a009e766d079a14569
Arin Bagul <prototypearin@gmail.com>
2025-07-08 04:13:46 +0530

    bug fix - removed from communitydetails.jsx

--- src/Pages/CommunityDetails/index.tsx
+++ src/Pages/CommunityDetails/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { color } from "react-native-tailwindcss";
 
 const CommunityDetails = () => {
   const darkThemeStyles = {
